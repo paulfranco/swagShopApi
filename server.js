19d90c4d1a39136e79290d79e5a546bee2054b33
Paul Franco <paulfranco@me.com>
2018-05-21 00:13:43 -0700

    Created the server shell for this project

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.listen(3000, function() {
+    console.log("Listening on port 3000...")
+});
